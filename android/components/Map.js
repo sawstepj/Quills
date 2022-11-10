5ff0831ede11c4be7b8fe10dd22a2b085974e11d
sawstepj <83564749+sawstepj@users.noreply.github.com>
2022-11-10 02:19:36 -0600

    Ability to place markers

--- android/components/Map.js
+++ android/components/Map.js
@@ -1,20 +1,60 @@
 import {StyleSheet, View} from 'react-native';
-import React from 'react';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import React, {useState} from 'react';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 
 export default function Map() {
+  const region = {
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.015,
+    longitudeDelta: 0.0121,
+  };
+
+  //get initial location
+
+  const [quills, setQuills] = useState([
+    {
+      coordinate: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+      },
+      title: 'Test',
+      description: 'Test',
+    },
+  ]);
+
+  // Add a marker to the map
+  const addMarker = e => {
+    setQuills([
+      ...quills,
+      {
+        coordinate: {
+          latitude: e.nativeEvent.coordinate.latitude,
+          longitude: e.nativeEvent.coordinate.longitude,
+        },
+      },
+    ]);
+  };
+
   return (
     <View style={styles.container}>
       <MapView
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        provider={PROVIDER_GOOGLE}
         style={styles.map}
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}
-      />
+        initialRegion={region}
+        onPress={e => {
+          addMarker(e);
+        }}>
+        {quills.map(quill => (
+          <Marker
+            key={quill.coordinate.latitude}
+            coordinate={quill.coordinate}
+            title={quill.title}
+            description={quill.description}
+            //add marker to array
+          />
+        ))}
+      </MapView>
     </View>
   );
 }
